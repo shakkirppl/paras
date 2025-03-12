573d50317e8b57b29f693e921a204e17f3e9ff4d
shakkir <shakkirppl@gmail.com>
2025-03-12 20:18:58 +0530

    customer update

--- app/Models/Customers.php
+++ app/Models/Customers.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Customers extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name',
+        'email',
+        'mobile ',
+        'gender',
+        'password',
+        'user_id',
+        'total_point',
+        'spend_point',
+        'current_point',
+        'total_purchase_amount'
+    ];
 }
